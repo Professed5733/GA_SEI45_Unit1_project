0034e0cd3c81aa017360f973c1642c367083661b
Firdaus_87 <mohamedf2_net@hotmail.com>
2023-07-11 21:45:07 +0800

    Update

--- stg/script.js
+++ stg/script.js
@@ -1,7 +1,3 @@
-const dino = document.getElementById("dino");
-const cactus = document.getElementById("cactus");
-const score = document.getElementById("score");
-
 function createGame() {
   const newGame = document.createElement("div");
   newGame.className = "game";
@@ -47,7 +43,7 @@ function createCactus() {
   newCactus.style.left = "580px";
   newCactus.style.backgroundImage = "url(img/cactus.png)";
   newCactus.style.backgroundSize = "20px 40px";
-  //   newCactus.style.animation = "block 1.5s infinite linear";
+  newCactus.style.animation = "block 1.5s infinite linear";
 
   newGame.appendChild(newCactus);
 }
@@ -77,12 +73,6 @@ function createScoreValue() {
   document.getElementById("scoreValue").innerHTML = "000000";
 }
 
-createGame();
-createDino();
-createCactus();
-createScore();
-createScoreValue();
-
 function updateScore() {
   const scoreElement = document.getElementById("scoreValue");
   let score = 0;
@@ -92,8 +82,31 @@ function updateScore() {
     scoreElement.innerHTML = `Score: ${score.toString().padStart(6, "0")}`;
   }, 1000);
 }
+const start = document.getElementsByClassName("start");
 
-updateScore();
+function startGame() {
+  if (!start.classList.contains("start")) {
+    start.classList.add("start");
+    start.style.display = "none";
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Space") {
+    startGame();
+  }
+});
+
+// createGame();
+// createDino();
+// createCactus();
+// createScore();
+// createScoreValue();
+// updateScore();
+
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+const score = document.getElementById("score");
 
 function jump(dino) {
   if (!dino.classList.contains("jump")) {
@@ -105,8 +118,33 @@ function jump(dino) {
   }
 }
 
-// document.addEventListener("keydown", function (event) {
-//   if (event.code === "Space") {
-//     jump();
-//   }
-// });
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Space") {
+    jump(dino);
+  }
+});
+
+function checkOverlap() {
+  // Get the elements or their positions
+  const object1 = document.getElementById("dino");
+  const object2 = document.getElementById("cactus");
+  const rect1 = object1.getBoundingClientRect();
+  const rect2 = object2.getBoundingClientRect();
+
+  // Check for overlap
+  if (
+    rect1.left < rect2.right &&
+    rect1.right > rect2.left &&
+    rect1.top < rect2.bottom &&
+    rect1.bottom > rect2.top
+  ) {
+    console.log("Collision");
+    // Perform collision-related actions here
+  }
+
+  // Call the function again on the next frame
+  requestAnimationFrame(checkOverlap);
+}
+
+// Start the continuous overlap checking
+requestAnimationFrame(checkOverlap);
